4f82d88188520bb70ef8cb1b66b99cdef5a9936d
Aklesh yadav <yaklesh802@gmail.com>
2024-02-21 11:15:35 +0530

    DataTransformation Config

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlproject import logger
 from mlproject.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlproject.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from mlproject.pipelines.stage_03_data_transformation import DataTransformationPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 
@@ -26,3 +27,12 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME="Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj=DataTransformationPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\n===============X")
+except Exception as e:
+    logger.exception(e)
---
+++ src/mlproject/components/data_transformation.py
@@ -0,0 +1,27 @@
+from mlproject import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+from mlproject.entity.config_entity import DataTransformationConfig
+
+
+
+class DataTransformation:
+    def __init__(self,config: DataTransformationConfig):
+        self.config=config
+
+
+    
+    def train_test_splitting(self):
+        data=pd.read_csv(self.config.data_path)
+        #splitting the data into training and the test set
+        train,test= train_test_split(data)
+        train.to_csv(os.path.join(self.config.root_dir,"train.csv"),index=False)
+        test.to_csv(os.path.join(self.config.root_dir,"test.csv"),index=False)
+
+        logger.info("Splitted data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
